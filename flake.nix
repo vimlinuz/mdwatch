4649b1b73c231f26d821306fd4b4af3e975c58d6
vimlinuz <vimlinuz@gmail.com>
2026-07-18 17:40:38 +0545

    flake: added treefmt for formatting

--- flake.nix
+++ flake.nix
@@ -4,14 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     naersk.url = "github:nix-community/naersk";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
-    { nixpkgs, naersk, ... }:
+    {
+      self,
+      treefmt-nix,
+      nixpkgs,
+      naersk,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       naerskLib = pkgs.callPackage naersk { };
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
     in
     {
       packages.${system}.default = naerskLib.buildPackage {
@@ -30,6 +38,7 @@
         nativeBuildInputs = [ pkgs.pkg-config ];
 
       };
-      formatter = pkgs.rustfmt;
+      formatter.${system} = treefmtEval.config.build.wrapper;
+      checks.${system}.formatting = treefmtEval.config.build.check self;
     };
 }
---
+++ treefmt.nix
@@ -0,0 +1,6 @@
+{
+  projectRootFile = "flake.nix";
+  programs.rustfmt.enable = true;
+  programs.prettier.enable = true;
+  programs.shfmt.enable = true;
+}
